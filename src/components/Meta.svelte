856be8150a226c58f56b499498bfe7a5c3b37ef7
Kevin Litman-Navarro <kevin@Kevins-MacBook-Pro-2.local>
2022-04-05 10:20:00 -0700

    update github pages

--- src/components/Meta.svelte
+++ src/components/Meta.svelte
@@ -35,7 +35,7 @@
 
   <meta name="robots" content="max-image-preview:large" />
 
-  <link rel="canonical" href="{url}/" />
+  <link rel="canonical" href="/static/assets/square.jpeg" />
 
   {#each preloadFont as href}
     <link rel="preload" {href} as="font" type="font/woff2" crossorigin />
