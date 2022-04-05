0feece73b2450e0fae46cdf44093b5e6d352463a
Kevin Litman-Navarro <kevin@Kevins-MacBook-Pro-2.local>
2022-04-05 10:40:00 -0700

    update github pages

--- refresh.sh
+++ refresh.sh
@@ -1,3 +1,3 @@
-/Users/kevin/.nvm/versions/node/v14.15.1/bin/node run gdoc
-/Users/kevin/.nvm/versions/node/v14.15.1/bin/node run build
+/Users/kevin/.nvm/versions/node/v14.15.1/bin/npm run gdoc
+/Users/kevin/.nvm/versions/node/v14.15.1/bin/npm run build
 make github
